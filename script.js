8967b47ef7a1dd0ba2a8a032dffa5f5126f20290
Glitch (hello-webpage) <none>
2020-05-15 23:24:21 +0000

    💑🍛 Checkpoint ./script.js:12897968/1867 ./take-action.html:12897968/9900 ./take-action.css:12897968/86

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function checkOffItem(clicked) {
 
 var actionListItem = document.getElementsByClassName("action-li");
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < actionListItem.length; i++) {
   var newSpan = document.createElement("span");
   var x = document.createTextNode("X");
   newSpan.appendChild(x);
@@ -61,53 +61,4 @@ function closeListItem() {
 }
 
 
-/************************************************/
-/************** CODE FOR EXTENSIONS *************/
-/************************************************/
 
-//UNCOMMENT TO ADD, COMPLETE, AND DELETE NEW ITEMS
-/* Add new items 
-var addButton = document.getElementById("add-button");
-addButton.addEventListener("click", newActionItem);
-
-function newActionItem() {
-  //Create a new "li" element to hold the action item you enter
-  var newListElement = document.createElement("li");
-
-  //Create a variable to select and store the value of what is entered into the input box
-  //WHAT IS .VALUE?!! Debug alert
-  var inputBoxValue = document.getElementById("task-entry-box").value;
-
-  //Add a variable that creates a new text node containing the value of the input box
-  var actionItemText = document.createTextNode(inputBoxValue);
-
-  //Append or attach the new list text you entered to the new "li" element you created
-  newListElement.appendChild(actionItemText);
-
-  if (inputBoxValue === " ") {
-    alert("Please enter your action item");
-  } else {
-    //Find and select the HTML ul element called my-list and attach the li
-    //element containing our action item text to the end of the list
-    document.getElementById("my-list").appendChild(newListElement);
-  }
-  //Pretty sure this will remove the text from the input box
-  document.getElementById("task-entry-box").value = "";
-
-  
-  //add delete button to each new item - still need to commemnt this
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "deleteButton";
-  span.appendChild(txt);
-  newListElement.appendChild(span);
-  
-  //ability to delete all new items
-  for (var i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-}
-*/
